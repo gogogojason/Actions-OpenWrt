122cc7c4998beca0d901ecb4e910a52592108ce8
icons <65128346+gogogojason@users.noreply.github.com>
2020-11-14 16:00:12 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -85,3 +85,4 @@ date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by hfy166'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 sed -i '$a cgi-timeout = 300' package/feeds/packages/uwsgi/files-luci-support/luci-webui.ini
+sed -i "s/192.168.1.1/192.168.2.1/g" package/base-files/files/bin/config_generate
